99d7e67deaba3dc54520778fb2dad592c5410ece
Dima Smaha <fannifty@gmail.com>
2022-10-27 14:46:24 +0300

    upd

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,17 +1,16 @@
-//Runner template for cucumber setup
+////Runner template for cucumber setup
 //package runner;
 //
-//import io.cucumber.java.After;
 //import io.cucumber.junit.Cucumber;
 //import io.cucumber.junit.CucumberOptions;
 //import org.junit.runner.RunWith;
 //
 //@RunWith (Cucumber.class)
-//@CucumberOptions(glue = "steps")
+//@CucumberOptions(
+//    features = {"src/test/java/features"},
+//    glue = {"src/test/java/steps"}
+//    )
 //
-//public class Runner extends pageObjects.commonPageObjects{
-//    @After("Close browser")
-//    public void close() {
-//        finish();
-//    }
+//public class Runner {
+//
 //}
--- target/test-classes/features/login.feature
+++
@@ -1,7 +0,0 @@
-Feature: Register user
-  Scenario: Successfully register user
-    Given Website is opened
-    When User clicks on register button
-    And User fills up the form
-    And User clicks submit button
-    Then Success register notification shown
